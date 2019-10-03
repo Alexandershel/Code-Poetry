fe7448795268481d942b003e4d415b10cf8de093
Shelby Alexander <43657371+Alexandershel@users.noreply.github.com>
2019-10-03 17:46:32 -0400

    Add files via upload

---
+++ Gratitude.cpp
@@ -0,0 +1,19 @@
+// Gratitude
+
+class gratitude();
+
+void main()
+{
+    string thanks = "thank you";
+    for(int j = 0; j < INFINITY; j++)
+    {
+        cout << thanks;
+    }
+}
+
+
+
+
+
+
+
