2dca7643f98b20611dbf6bc3310bf284478e8c8b
Shelby Alexander <43657371+Alexandershel@users.noreply.github.com>
2019-10-17 18:40:41 -0400

    Add files via upload

---
+++ Hope.cpp
@@ -0,0 +1,22 @@
+// Pandora's Box
+
+class box() 
+{
+	Public:
+		string war, crime, famine, spite,
+			pestilience, sickness, malice,
+			envy, woe, wickedness, cruelty,
+			hope;
+} contents;
+
+void main()
+{
+	for(;;)
+	{
+		throw contents[]();
+		if(contents == hope)
+		{
+			break();
+		}
+	}
+}
